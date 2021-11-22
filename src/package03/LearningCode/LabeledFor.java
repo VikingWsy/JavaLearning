a1d3f70a77cfdadb21503d854bf4343438ccda9b
Vikingwangsyx <viking.wangsy@outlook.com>
2021-11-22 11:41:51 +0800

    test

--- src/package03/LearningCode/LabeledFor.java
+++ src/package03/LearningCode/LabeledFor.java
@@ -1,7 +1,25 @@
 package package03.LearningCode;
 
-public class LabeledFor {
-    public static void [String[] args]{
+import package03.PlantTools.PrintOptions;
 
+public class LabeledFor {
+    public static void main(String[] args){
+        int i = 0;
+        outer:
+        for (;true;){
+            inner:
+            for ( ; i < 10; i++ ){
+                PrintOptions.print( "i", i );
+                if ( i == 2 ) {
+                    PrintOptions.prstg("Continue", "Executed");
+                    continue;
+                }
+                else if ( i == 3 ) {
+                    PrintOptions.prstg("Break", "Executed");
+                    break;
+                }
+            }
+
+        }
     }
 }
