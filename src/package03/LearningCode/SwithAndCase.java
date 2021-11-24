2b7ea004dce4eb2b72400f0b789241e5f0338518
Vikingwang <viking.wangsy@gmail.com>
2021-11-24 14:13:55 +0800

    JavaLearning

--- src/package03/LearningCode/SwithAndCase.java
+++ src/package03/LearningCode/SwithAndCase.java
@@ -1,4 +1,22 @@
 package package03.LearningCode;
 
+import package03.PlantTools.PrintOptions;
+
 public class SwithAndCase {
+    public static void main( String[] args ){
+        char c = ( char ) ( Math.random() * 26 + 'a' );
+
+        double d = Math.random();
+        d = d * 26;
+        d = d + 'a';
+        PrintOptions.prchr( "Test c + :", c );
+        switch ( c ){
+            case 'a': PrintOptions.prstg( "a", "is executed"); break;
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u': PrintOptions.prstg( "u", "is executed"); break;
+            default: PrintOptions.prstg( "Default", "is executed");
+        }
+    }
 }
