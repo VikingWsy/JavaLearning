63b684fa47d580de70699463373b9162a3808052
Vikingwang <viking.wangsy@gmail.com>
2022-01-10 14:32:35 +0800

    JavaLearning

--- src/Package04/LearningCode/Arrays_01.java
+++ src/Package04/LearningCode/Arrays_01.java
@@ -6,9 +6,11 @@ public class Arrays_01 {
     public static void main(String[] args){
         int[] a1 = {1, 2, 3, 4, 5};
         int[] a2 = a1;
-        for ( int i = 0; i < a2.length; i++)
+        for ( int i = 0; i < a2.length; i++) {
             a2[i]++;
-        for ( int i = 0; i < a1.length; i++ )
-            PrintOptions.prstring("a1[" + i + "] = " + a1[i] );
+        }
+        for ( int i = 0; i < a1.length; i++ ) {
+            PrintOptions.prstring("a1[" + i + "] = " + a1[i]);
+        }
     }
 }
